9667dd09d371a7e02a1c2f14dbd986df47f64e1d
Felipe Gomes Duarte <felipegdmito@gmail.com>
2017-11-26 03:09:41 -0200

    Projeto inteiro.
    Por se tratar de um exercício, forcei a desorganização a fim de testar algumas possibilidades(principalmente a de uso do ".." para uso de pastas anteriores)

---
+++ JavaScript/Exercicio 1/script/javascript.js
@@ -0,0 +1,33 @@
+
+/*Comentário*/
+//Primeira aula de javascript
+
+//document.getElementById("ID").src = variavel;
+//var  segundaVariavel=10;
+segundaVariavel = 10
+const primeiraConstante = 1;
+
+
+
+primeiraVariavel = "Esta é nossa primeira variável.";
+
+alert(primeiraVariavel);
+alert(segundaVariavel);
+
+
+
+/*
+switch (farol) {
+	case "vermelho":
+		alert("Pare")
+		break
+	case "amarelo":
+		alert("Atencao")
+		break
+	case "verde":
+		alert("Prossiga")
+		break
+	default:
+		alert("Cor ilegal")
+}
+*/
